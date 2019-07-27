2cbada77361dd97341b694b7fa593bb415ea3bdb
John Donaghy <johnfdonaghy@gmail.com>
2019-07-27 17:45:30 -0500

    RPi_utils/Makefile

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -12,54 +12,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-     
-     
+#include <iostream>
+
+#include <execinfo.h>
+#include <signal.h>
+#include <pthread.h>
+
+using namespace std;
+
 RCSwitch mySwitch;
  
+int PIN = 4;
+int pulseLength = 0;
 
-
-int main(int argc, char *argv[]) {
-  
-     // This pin is not the first pin on the RPi GPIO header!
-     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
-     // for more information.
-     int PIN = 2;
-     
-     if(wiringPiSetup() == -1) {
-       printf("wiringPiSetup failed, exiting...");
-       return 0;
-     }
-
-     int pulseLength = 0;
-     if (argv[1] != NULL) pulseLength = atoi(argv[1]);
-
-     mySwitch = RCSwitch();
-     if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
-     mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
-     
-    
-     while(1) {
-  
+void * callback(void * arg) {
       if (mySwitch.available()) {
-    
         int value = mySwitch.getReceivedValue();
     
         if (value == 0) {
           printf("Unknown encoding\n");
         } else {    
-   
           printf("Received %i\n", mySwitch.getReceivedValue() );
         }
     
         fflush(stdout);
         mySwitch.resetAvailable();
       }
-      usleep(100); 
-  
-  }
+      return NULL;
+}
 
-  exit(0);
+int setup() {
+  // This pin is not the first pin on the RPi GPIO header!
+     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+     // for more information.
+     
+     if(wiringPiSetup() == -1) {
+       printf("wiringPiSetup failed, exiting...");
+       return 0;
+     }
 
+     mySwitch = RCSwitch();
+     if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
+     mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
+     mySwitch.setReceiveCallback(callback);   
+     return 1;
+}
 
+int main(int argc, char *argv[]) {
+    if (argv[1] != NULL) pulseLength = atoi(argv[1]);
+    setup(); 
+    while (true)
+      sleep(10000000); 
+    exit(0);
 }
 
